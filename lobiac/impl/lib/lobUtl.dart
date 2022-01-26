01a06e662b88c34cf3f050a86ddb070140229a41
halka9000stg <csta19097@g.nihon-u.ac.jp>
2022-01-26 17:21:06 +0900

    add: Lobiacのマジックナンバー検査

--- lobiac/impl/lib/lobUtl.dart
+++ lobiac/impl/lib/lobUtl.dart
@@ -19,6 +19,36 @@ List<int> lobMagic(int ver){
   return lobHex;
 }
 /**
+Lobiacのマジックナンバーと合致しているか検査する。合致していれば非負intでバージョン値、合致していなければエラーコード。
+- -9: 引数のリストがマジックナンバーのバイト数に足りない
+- -2: バージョンのパースに失敗した
+- -1: 固定部が合致しない
+ */
+int matchLobMagic(List<int> src){
+  if(src.length < 8){
+    return -9;
+  }
+  List<int> magic = [];
+  if(src.length > 8){
+    magic = src.take(8).toList();
+  }else{
+    magic = src;
+  }
+  if(magic.take(5).toList() == [108+105,111+97,].addAll(ascii.encode("lob").toList())){
+    if(magic.last ==98+99){
+      List<int> nrCand = magic.skip(5).take(2).toList();
+      String asciiStr = ascii.decode(nrCand, allowInvalid: true);
+      try{
+        int ver = inr.parse(asciiStr);
+        return ver;
+      }catch (e){
+        return -2;
+      }
+    }
+  }
+  return -1;
+}
+/**
 List\<int\>からHexStrへの変換
  */
 extension HexUtl4int on List<int>{
